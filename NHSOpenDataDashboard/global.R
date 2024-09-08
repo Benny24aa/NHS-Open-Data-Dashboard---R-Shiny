6d43d182bc9a4383461dd32c5a91eaeb5faafd54
Ben Harley <harleyb101010@gmail.com>
2024-09-09 00:14:05 +0100

    data zone and interminate zones lookups added - int zone, ca and hb codes kept in for future left_join when necessary

--- NHSOpenDataDashboard/global.R
+++ NHSOpenDataDashboard/global.R
@@ -40,6 +40,8 @@ library(sf)
 
 ####### Necessary Lookup Files ########
 HB_Lookup <- read.csv("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/Lookups/hb lookup.csv")
+Council_Lookup <- read.csv("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/Lookups/council lookup.csv")
+Interminate_Zone_Lookup <- read.csv("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/Lookups/iz2011_codes_and_labels_14072022.csv")
 
 ####### Data Cleaning to speed up processes when called upon by server ########
 
@@ -54,3 +56,11 @@ Council_Lookup_Cleaned <- Council_Lookup |>
   select(CA,CAName,CADateArchived)|>
   filter(is.na(CADateArchived))|>
   select(-CADateArchived)
+
+# Interminate Zone Lookup File
+Interminate_Zone_Lookup_Cleaned <- Interminate_Zone_Lookup|>
+  select(IntZone,IntZoneName,CA, HB) #CA and HB codes kept for future left_join when needed and to speed up app by removing unnecessary data when not needed.
+
+# Data Zone Lookup File
+Data_Zone_Lookup_Cleaned <- Data_Zone_Lookup |>
+  select(DataZone, DataZoneName, IntZone, CA, HB) #Int_Zone, CA and HB codes kept for future left_join when needed and to speed up app by removing unnecessary data when not needed.
