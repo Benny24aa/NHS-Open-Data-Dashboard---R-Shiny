d2535f1451147a337781cd3622ec90de5e8991f6
Ben Harley <harleyb101010@gmail.com>
2024-09-09 00:49:42 +0100

    added xlsxl write functions

--- Scripts for Tableau/geo file cleans for tableau dashboard.R
+++ Scripts for Tableau/geo file cleans for tableau dashboard.R
@@ -55,20 +55,32 @@ HB_Lookup_Cleaned <- HB_Lookup |>
   filter(is.na(HBDateArchived))|>
   select(-HBDateArchived)
 
+write_xlsx(HB_Lookup_Cleaned,"C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/Lookup Files for Tableau/HB_Tableau.xlsx")
+
 # Council Lookup File
 Council_Lookup_Cleaned <- Council_Lookup |>
   select(CA,CAName,CADateArchived)|>
   filter(is.na(CADateArchived))|>
   select(-CADateArchived)
 
+write_xlsx(Council_Lookup_Cleaned,"C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/Lookup Files for Tableau/Council_Tableau.xlsx")
+
+
 # Interminate Zone Lookup File
 Interminate_Zone_Lookup_Cleaned <- Interminate_Zone_Lookup|>
   select(IntZone,IntZoneName,CA, HB) #CA and HB codes kept for future left_join when needed and to speed up app by removing unnecessary data when not needed.
+
+write_xlsx(Interminate_Zone_Lookup_Cleaned,"C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/Lookup Files for Tableau/Interm_Tableau.xlsx")
 
 # Data Zone Lookup File
 Data_Zone_Lookup_Cleaned <- Data_Zone_Lookup |>
   select(DataZone, DataZoneName, IntZone, CA, HB) #Int_Zone, CA and HB codes kept for future left_join when needed and to speed up app by removing unnecessary data when not needed.
 
+write_xlsx(Data_Zone_Lookup_Cleaned,"C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/Lookup Files for Tableau/DataZone_Tableau.xlsx")
+
+
 # Hospital Lookup File
 Hospital_Lookup_Cleaned <- Hospital_Lookup |>
   select(HospitalCode, HospitalName, HealthBoard, CouncilArea, IntermediateZone, DataZone) ### Kept codes for future left_join when needed
+
+write_xlsx(Hospital_Lookup_Cleaned,"C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/Lookup Files for Tableau/Hospitals_Tableau.xlsx")
