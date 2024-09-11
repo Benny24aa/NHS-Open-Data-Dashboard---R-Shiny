11c5ad0ec7951ee63347c87a1e27b26df35796d1
Ben Harley <harleyb101010@gmail.com>
2024-09-11 18:26:30 +0100

    Sourcing added for reference file section

--- NHSOpenDataDashboard/ui.R
+++ NHSOpenDataDashboard/ui.R
@@ -2,11 +2,12 @@
 
 source("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/ui (sources)/Information Tab UI Code.R"
 )# Sourcing in Information Tab from different script
-
+source("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/ui (sources)/Reference File Tab UI Code.R") # Sourcing in Reference File UI tab script
 
 
 navbarPage(title =span( "NHS Open Data Application", style = "blackground-color: #dedef7", style="color:red"),
-           information ### Information Tab being called on by UI
+           information, ### Information Tab being called on by UI
+           referencefile
 
 
 
