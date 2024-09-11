540c4237343150fa074486389b04c9ddc6c8fce5
Ben Harley <harleyb101010@gmail.com>
2024-09-11 18:30:39 +0100

    Sourcing file for reference files section setup and ready to go

--- NHSOpenDataDashboard/server.R
+++ NHSOpenDataDashboard/server.R
@@ -1,4 +1,13 @@
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-  source(file.path("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/server (sources)/Information Tab Script Server.R"), local = TRUE)}$value
+  source(file.path("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/server (sources)/Information Tab Script Server.R"), local = TRUE)$value
+  source(file.path("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/server (sources)/Reference File Script Server.R"))
+  
+  
+  
+  
+  
+  
+  } #End of Server
+  
