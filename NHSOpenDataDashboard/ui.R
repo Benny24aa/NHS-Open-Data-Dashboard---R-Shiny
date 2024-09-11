692e94f60718019a819accab81e4347638e1962a
Ben Harley <harleyb101010@gmail.com>
2024-09-11 17:23:14 +0100

    Information tab and navbarpage created so user can use top bar to navigate - design still being considered

--- NHSOpenDataDashboard/ui.R
+++ NHSOpenDataDashboard/ui.R
@@ -1,33 +1,17 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+### Sourcing Scripts for Use for UI
 
-library(shiny)
+source("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/ui (sources)/Information Tab UI Code.R"
+)# Sourcing in Information Tab from different script
 
-# Define UI for application that draws a histogram
-fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+navbarPage(title =span( "NHS Open Data Application", style = "blackground-color: #dedef7", style="color:red"),
+           information ### Information Tab being called on by UI
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-)
+
+
+
+
+
+
+)#End of navbarpage
