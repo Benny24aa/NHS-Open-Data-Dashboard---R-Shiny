855c0b079939c6ba06fdc8fb766dbaf868d45ad8
Ben Harley <harleyb101010@gmail.com>
2024-09-14 17:08:44 +0100

    shorten code

--- server (sources)/Reference File Script Server.R
+++ server (sources)/Reference File Script Server.R
@@ -1,40 +1,42 @@
-##### Creating a situation so that all lookup files appear in the same datatable prepared for download
+##### Creating a situation so that all look up files appear in the same datatable prepared for download
 
 
 data_table <- reactive({
   
-  table_data <- switch(input$reference_file_select,
-                            "HB_Lookup_Cleaned" = HB_Lookup_Cleaned,
+  table_data <- switch(input$ref_select,
+                            "HB_Lookup" = HB_Lookup,
                             "Council_Lookup_Cleaned" = Council_Lookup_Cleaned,
                             "Interminate_Zone_Lookup_Cleaned" = Interminate_Zone_Lookup_Cleaned,
                             "Data_Zone_Lookup_Cleaned" = Data_Zone_Lookup_Cleaned,
                             "Hospital_Lookup_Cleaned" = Hospital_Lookup_Cleaned)
 })
 
-# Render Data Table ----
+# Render Data Table 
 
-output$reference_file_filter_download <- DT::renderDataTable({
+output$table_filtered <- DT::renderDataTable({
   
-  table_colnames <- gsub("_", "", colnames(data_table()))
+  # Remove the underscore from column names in the table
+  table_colnames  <-  gsub("_", " ", colnames(data_table()))
   
-  DT::datatable(datatable(),
-                style = 'bootstrap',
+  DT::datatable(data_table(), style = 'bootstrap',
                 class = 'table-bordered table-condensed',
                 rownames = FALSE,
                 options = list(pageLength = 20,
                                dom = 'tip',
                                autoWidth = TRUE),
                 filter = "top",
-                colnames = table_colnames )
+                colnames = table_colnames)
+  
 })
 
-# Enable data to be downloaded from table
 
-output$reference_file_download_csv <- downloadHandler(
+# Download Data 
+output$download_table_csv <- downloadHandler(
   filename = function() {
-    paste(input$reference_file_select, ".csv", sep = "")
+    paste(input$ref_select, "_data.csv", sep = "")
   },
   content = function(file) {
-    write_csv(datatable()[input[["reference_file_filter_download_rows_all"]],], file)
-    }
+    # This downloads only the data the user has selected using the table filters
+    write_csv(data_table()[input[["table_filtered_rows_all"]], ], file) 
+  } 
 )
