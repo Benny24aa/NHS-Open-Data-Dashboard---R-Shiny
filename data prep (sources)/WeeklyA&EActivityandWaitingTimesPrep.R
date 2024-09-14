918a4bbd8c6d565f7e0259a39c8d315bcb86ee8e
Ben Harley <harleyb101010@gmail.com>
2024-09-14 17:38:29 +0100

    open api data brought into the dashboard for a&e

---
+++ data prep (sources)/WeeklyA&EActivityandWaitingTimesPrep.R
@@ -0,0 +1,3 @@
+### Weekly Accident and Emergency Data taken from Open Data through PHS Open Data Library
+weeklyaccidentandemergencydata <- get_resource(res_id = "a5f7ca94-c810-41b5-a7c9-25c18d43e5a4")
+
