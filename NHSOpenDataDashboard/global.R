f038f9fc0fbea1cbc1855ac4aff909375681d02f
Ben Harley <harleyb101010@gmail.com>
2024-09-14 14:33:52 +0100

    new libraries and new ref_file_list so file names appear with no spaces in filter

--- NHSOpenDataDashboard/global.R
+++ NHSOpenDataDashboard/global.R
@@ -37,7 +37,17 @@ library(tmap)
 library(tmaptools)
 library(sf)
 library(phsopendata)
+library(plotly)
+library(crosstalk)
+library(lubridate)
+library(kableExtra)
+### Filter Changes
 
+ref_file_list <- c("HB Lookup" =" HB_Lookup_Cleaned",
+                            "Council Lookup" = "Council_Lookup_Cleaned",
+                            "Interminate Zone Lookup" = "Interminate_Zone_Lookup_Cleaned",
+                            "Data Zone Lookup" = "Data_Zone_Lookup_Cleaned",
+                            "Hospital Lookup" = "Hospital_Lookup_Cleaned")
 
 #### Geo Lookup Script ####
 source(file.path('C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/data prep (sources)/Geo Lookup Source Script.R'),  local = TRUE)$value 
