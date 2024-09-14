9779bab3283643aed05ea0ba44b7c8f7932728c5
Ben Harley <harleyb101010@gmail.com>
2024-09-14 15:27:09 +0100

    added title and style to dashboard navbarpage

--- NHSOpenDataDashboard/ui.R
+++ NHSOpenDataDashboard/ui.R
@@ -4,8 +4,16 @@ source("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS
 )# Sourcing in Information Tab from different script
 source("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/ui (sources)/Reference File Tab UI Code.R") # Sourcing in Reference File UI tab script
 
-
-navbarPage(title =span( "NHS Open Data Application", style = "blackground-color: #dedef7", style="color:red"),
+navbarPage(title = div(tags$a(img(src="", width=120, alt = ""),
+                              href= "",
+                              target = "_blank"),
+                       style = "position: relative; top: -10px;"),
+           windowTitle = "NHS Open Data Application", #title for browser tab
+           header = tags$head(includeCSS("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/www/styles.css"), # CSS styles
+                              HTML("<html lang='en'>")),
+           
+           ##### Tab Panels
+           
            information, ### Information Tab being called on by UI
            referencefile
 
