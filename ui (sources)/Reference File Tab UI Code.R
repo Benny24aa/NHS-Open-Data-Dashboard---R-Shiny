c0ee3f33764e90b925ff28e3771cc15df12f0297
Ben Harley <harleyb101010@gmail.com>
2024-09-14 15:37:59 +0100

    Color change committed throughout dashboard

--- ui (sources)/Reference File Tab UI Code.R
+++ ui (sources)/Reference File Tab UI Code.R
@@ -1,6 +1,6 @@
 referencefile <-
   tabPanel(title = "Reference Files", icon = icon('table'),
-           h2("Select the data you wish to download", style = "color: #3F3685 ; font-weight: 600"),
+           h2("Select the data you wish to download", style = "color: #336699 ; font-weight: 600"),
            p("This section allows you to view reference file data in table format. You can use the filters to select the data you're interested in and download it into a csv format using the download button."),
            fluidRow(
              column(6, selectInput(inputId = "reference_file_select",
