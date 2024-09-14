a3456af50afec5bf245e94f9c0d446b0d7146400
Ben Harley <harleyb101010@gmail.com>
2024-09-14 14:34:05 +0100

    minor changes to server file

--- NHSOpenDataDashboard/server.R
+++ NHSOpenDataDashboard/server.R
@@ -1,9 +1,8 @@
 
 # Define server logic required to draw a histogram
-function(input, output, session) {
+function(input, output) {
   source(file.path("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/server (sources)/Information Tab Script Server.R"), local = TRUE)$value
-  source(file.path("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/server (sources)/Reference File Script Server.R"))
-  
+  source(file.path("C:/Users/harle/OneDrive/Desktop/NHS Open Data R App/Data Prep Respos/NHS-Open-Data-Dashboard---R-Shiny/server (sources)/Reference File Script Server.R"), local = TRUE)$value
   
   
   
