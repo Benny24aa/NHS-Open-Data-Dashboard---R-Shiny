bf7ddb6a909bd67ff9930cf7503a1818d56d7ffd
Ben Harley <harleyb101010@gmail.com>
2024-09-17 19:43:01 +0100

    Left joins added so file is prepared for use on dashboard

--- data prep (sources)/WeeklyA&EActivityandWaitingTimesPrep.R
+++ data prep (sources)/WeeklyA&EActivityandWaitingTimesPrep.R
@@ -5,3 +5,10 @@ weeklyaccidentandemergencydata_cleaned <- weeklyaccidentandemergencydata |>
   select(-Country,-DepartmentType)|> ### Removed both columns as both have the same value for all rows
   rename(HospitalCode = TreatmentLocation)|> ### To allow for future left_join with Hospital reference file
   rename(HB = HBT) ### To allow for future left_join with HBT reference file
+ 
+
+weeklyaccidentandemergencydata_cleaned <- left_join(weeklyaccidentandemergencydata_cleaned, Hospital_Lookup_Final, by = "HospitalCode") %>%  ### Left Join for Hospital Names
+  select(-HospitalCode) #### Removing hospital code as no longer needed anymore
+
+weeklyaccidentandemergencydata_cleaned <- left_join(weeklyaccidentandemergencydata_cleaned, HB_Lookup_Cleaned, by = "HB") %>% #### left join for Healthcode Name
+  select(-HB) #### Removing HB code as not needed anymore
