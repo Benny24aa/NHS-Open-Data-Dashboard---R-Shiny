9d5a53456f4cd639dac41d308e4c3602f6919b25
Ben Harley <harleyb101010@gmail.com>
2024-09-29 19:37:43 +0100

    Some grammar issues addressed

--- ui (sources)/A&E Healthboard Breakdown UI Code.R
+++ ui (sources)/A&E Healthboard Breakdown UI Code.R
@@ -1,7 +1,7 @@
 Accident_Emergency_Healthboard_Breakdown <- tabPanel(title = "Health Board Breakdown", 
                         icon = icon("hospital"),
                         h2("Weekly Breakdown of Accident and Emergency Data at Health Board Level", style = "color: #336699 ; font-weight: 600"),
-                        p("This is the new weekly Accident & Emergency (A&E) open data area. From 2 May 2023 the weekly attendance and waiting times statistics at Emergency Departments (EDs) across Scotland "),
+                        p("This is the new weekly Accident & Emergency (A&E) open data area. From 2015 the weekly attendance and waiting times statistics at Emergency Departments (EDs) across Scotland "),
 
                         fluidRow(
                         column(3, selectInput(inputId = "hb_ae_input", label = "select HB", choices = unique(total_ae_episodes_healthboard_level_cleaned$HBName)))),
