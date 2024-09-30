336534af065470bc55bc75fc517efadc037ab389
Ben Harley <harleyb101010@gmail.com>
2024-10-01 00:27:57 +0100

    type filter added

--- server (sources)/A&E Healthboard Breakdown Script Server.R
+++ server (sources)/A&E Healthboard Breakdown Script Server.R
@@ -2,21 +2,22 @@
 
 ##### Total AE attends server filter and plot
 
-filter_healthboard_ae <- reactive({
+filter_healthboard_ae_type  <- reactive({
   
-  filter_healthboard_ae <-  merged_ae_data_4_day [merged_ae_data_4_day$HBName == input$hb_ae_input,]
+  filter_healthboard_ae_type <- merged_ae_data[merged_ae_data$type == input$type_ae_input,]
   
-  return(filter_healthboard_ae)
+  return(filter_healthboard_ae_type)
   
 })
 
 output$total_ae_attend_by_hb <- renderPlotly({
   
-  plot <- plot_ly(data = filter_healthboard_ae(),
+  plot <- plot_ly(data = filter_healthboard_ae_type(),
                   x = ~ WeekEndingDate,
                   y = ~ totalseen,
-                  color = ~HBName,
+                  color = ~ HBName,
                   type = 'scatter',
                   mode = 'lines',
                   orientation = 'h')
 })
+
