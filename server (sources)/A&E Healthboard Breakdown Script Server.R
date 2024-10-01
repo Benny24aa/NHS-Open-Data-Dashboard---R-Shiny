7b9a94bda7eb0846c99837636b6d4b04531cb2c6
Ben Harley <harleyb101010@gmail.com>
2024-10-01 01:52:33 +0100

    Rate graph added to server for ae

--- server (sources)/A&E Healthboard Breakdown Script Server.R
+++ server (sources)/A&E Healthboard Breakdown Script Server.R
@@ -4,7 +4,7 @@
 
 filter_healthboard_ae_type  <- reactive({
   
-  filter_healthboard_ae_type <- merged_ae_data[merged_ae_data$type == input$type_ae_input,]
+  filter_healthboard_ae_type <- merged_ae_data_full_data[merged_ae_data_full_data$type == input$type_ae_input,]
   
   return(filter_healthboard_ae_type)
   
@@ -21,3 +21,13 @@ output$total_ae_attend_by_hb <- renderPlotly({
                   orientation = 'h')
 })
 
+output$total_ae_attend_by_hb_Rates <- renderPlotly({
+  
+  plot <- plot_ly(data = filter_healthboard_ae_type(),
+                  x = ~ WeekEndingDate,
+                  y = ~ Rate,
+                  color = ~ HBName,
+                  type = 'scatter',
+                  mode = 'lines',
+                  orientation = 'h')
+})
