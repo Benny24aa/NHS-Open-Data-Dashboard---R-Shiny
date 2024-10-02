584c45286aeec23aeb19e9570dc0c3c721a551ba
Ben Harley <harleyb101010@gmail.com>
2024-10-02 21:56:55 +0100

    Plotly graphs for ae age to UI with filter, some text updates to ui as well

--- ui (sources)/A&E Healthboard Breakdown UI Code.R
+++ ui (sources)/A&E Healthboard Breakdown UI Code.R
@@ -1,17 +1,31 @@
-Accident_Emergency_Healthboard_Breakdown <- tabPanel(title = "Health Board Breakdown", 
+Accident_Emergency_Healthboard_Breakdown <- tabPanel(title = "Health Board Breakdowns", 
                         icon = icon("hospital"),
                         h1("Weekly Breakdown of Accident and Emergency Data at Health Board Level", style = "color: #336699 ; font-weight: 600"),
-                        p("This is the new weekly Accident & Emergency (A&E) open data area. From 2015 the weekly attendance and waiting times statistics at Emergency Departments (EDs) across Scotland "),
+                        p("Coming soon"),
                         fluidRow(
                           column(3, selectInput(inputId = "type_ae_input", label = "Select type of data you wish to see", choices = unique(merged_ae_data_full_data$type)))),
                         
                         h3("Accident and Emergency - Number seen within selected time period", style = "color: #336699 ; font-weight: 600"),
-                        
+                        p("Coming soon"),
                         fluidRow(
                           column(3, plotlyOutput("total_ae_attend_by_hb", width = "300%", height = "600px"))),
                         
                         h3("Accident and Emergency - People per 100k rate seen within selected time period", style = "color: #336699 ; font-weight: 600"),
+                        p("Coming soon"),
+                        fluidRow(
+                          column(3, plotlyOutput("total_ae_attend_by_hb_Rates", width = "300%", height = "600px"))),
+                        
+                        h1("Monthly Demographic Data for Accident and Emergency Attendance across Scotland", style = "color: #336699 ; font-weight: 600"),
+                        p("Coming soon"),
+                        
+                        h3("Accident and Emergency Attendances by Age Group", style = "color: #336699 ; font-weight: 600"),
+                      
+                        fluidRow(
+                          column(3, selectInput(inputId = "ae_age_hb_input", label = "Select the healthboard you want to see data for", choices = unique(ae_age_analysis$HBName)))),
                         
                         fluidRow(
-                          column(3, plotlyOutput("total_ae_attend_by_hb_Rates", width = "300%", height = "600px")))
+                          column(3, plotlyOutput("total_ae_attend_by_age", width = "300%", height = "600px")))
+                        
+                        
+                        
                         ) #End of TabPanel
