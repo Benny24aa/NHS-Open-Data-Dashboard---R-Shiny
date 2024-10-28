c29a219934d21f9feb9718b713fd26556a6d5464
Benny24aa <harleyb101010@gmail.com>
2024-10-28 21:32:39 +0000

    leaflet output added

--- NHSOpenDataDashboard/ui (sources)/Diagnostic Waiting Times Code/diagnostic waiting times ui.R
+++ NHSOpenDataDashboard/ui (sources)/Diagnostic Waiting Times Code/diagnostic waiting times ui.R
@@ -1,5 +1,8 @@
 Diagnostics_section <- tabPanel(title = "Healthboard Breakdown", 
                                                      icon = icon("hospital"),
                                                      h1("Monthly Diagnostic Waiting Times at Health Board Level", style = "color: #336699 ; font-weight: 600"),
-                                                     p("Coming soon"))
+                                                     p("Coming soon"),
+                                leafletOutput(outputId = "Diagnostics_Map")
+                                
+                                )
 
