7a29c9a67ac5f18a231fe9d70ee8ccc851698353
Benny24aa <harleyb101010@gmail.com>
2024-10-28 20:27:41 +0000

    UI diagnostic start up

---
+++ NHSOpenDataDashboard/ui (sources)/Diagnostic Waiting Times Code/diagnostic waiting times ui.R
@@ -0,0 +1,5 @@
+Diagnostics_section <- tabPanel(title = "Healthboard Breakdown", 
+                                                     icon = icon("hospital"),
+                                                     h1("Monthly Diagnostic Waiting Times at Health Board Level", style = "color: #336699 ; font-weight: 600"),
+                                                     p("Coming soon"))
+
