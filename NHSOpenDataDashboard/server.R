4b149a5c679f24ad155843ce5e17a0d17d2104c9
Benny24aa <harleyb101010@gmail.com>
2024-10-28 21:31:42 +0000

    added in server source for diagnostic waiting times

--- NHSOpenDataDashboard/server.R
+++ NHSOpenDataDashboard/server.R
@@ -4,7 +4,7 @@ function(input, output) {
   source(file.path("server (sources)/Information Tab Script Server.R"), local = TRUE)$value
   source(file.path("server (sources)/Reference File Script Server.R"), local = TRUE)$value
   source(file.path("server (sources)/A&E Healthboard Breakdown Script Server.R"), local = TRUE)$value
-  
+  source(file.path("server (sources)/diagnostic waiting times server.R"), local = TRUE)$value
   
   
   
