035456bf12ac488c56a351499030c21aafe58d16
Benny24aa <harleyb101010@gmail.com>
2024-11-03 13:33:35 +0000

    added sourcing for landing page servers

--- NHSOpenDataDashboard/server.R
+++ NHSOpenDataDashboard/server.R
@@ -1,11 +1,10 @@
 
 # Define server logic required to draw a histogram
 function(input, output) {
-  source(file.path("server (sources)/Information Tab Script Server.R"), local = TRUE)$value
-  source(file.path("server (sources)/Reference File Script Server.R"), local = TRUE)$value
-  source(file.path("server (sources)/A&E Healthboard Breakdown Script Server.R"), local = TRUE)$value
-  
-  
+  source(file.path("server (sources)/Information Tab Script Server.R"), local = TRUE)$value ## Information page sourcing in
+  source(file.path("server (sources)/Reference File Script Server.R"), local = TRUE)$value # Reference file sourcing in
+  source(file.path("server (sources)/A&E Healthboard Breakdown Script Server.R"), local = TRUE)$value # AE healthboard script source in
+  source(file.path("landing pages/Landing Pages Server.R"), local = TRUE)$value # Landing Page source in
   
   
   } #End of Server
