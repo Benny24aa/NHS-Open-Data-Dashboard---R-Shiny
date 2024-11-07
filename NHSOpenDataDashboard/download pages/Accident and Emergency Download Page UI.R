c714e99303d259666ac00be9d802ee500fcb4d31
Benny24aa <harleyb101010@gmail.com>
2024-11-07 15:23:09 +0000

    download page for accident and emergency

---
+++ NHSOpenDataDashboard/download pages/Accident and Emergency Download Page UI.R
@@ -0,0 +1,9 @@
+Accident_Emergency_Download_Page <- tabPanel(title = "Download Data", icon = icon('table'),
+           h2("Select the Reference File data you wish to download", style = "color: #336699 ; font-weight: 600"),
+           p("This section allows you to view reference file data in table format. You can use the filters to select the data you're interested in and download it into a csv format using the download button."),
+           column(6, selectInput("acc_emer_select,", "Select the data you want to explore.",
+                                 choices = accident_emergency_download_list)),
+           mainPanel(width = 12,
+                     DT::dataTableOutput("accident_emergency_table_filtered")),
+           column(6, downloadButton('download_accident_emergency_table_csv', 'Download data')),
+  ) 
