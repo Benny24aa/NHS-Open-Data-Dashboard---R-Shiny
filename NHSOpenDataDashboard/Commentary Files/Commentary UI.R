81fc1f26df86c8dc273a0dc9032cd3096edca8cf
Benny24aa <harleyb101010@gmail.com>
2024-11-17 22:42:11 +0000

    added first parts of A&E updates page

--- NHSOpenDataDashboard/Commentary Files/Commentary UI.R
+++ NHSOpenDataDashboard/Commentary Files/Commentary UI.R
@@ -12,7 +12,12 @@ Commentary <- tabPanel(title = "Commentary and Updates",
                                     
                                     conditionalPanel(
                                       condition= 'input.com_select == "aecom"',
-                                      
+                                      p(h3("17/11/2024")),
+                                      h4("Accident and Emergency Healthboard Section "),
+                                      tags$ul(
+                                        tags$li("Graphs added for age, gender, and deprivation quintiles for accident and emergency admissions."),
+                                        tags$li("Download data tab added to drop down list for accident and emergency data"),
+                                        tags$li("Landing Page added for accident and emergency tab"))
                                     ) # end of conditional panel
                                     
                           ))) #End of TabPanel
