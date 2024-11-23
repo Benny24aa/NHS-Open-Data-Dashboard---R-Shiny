d0432176a756dfb9fdfd1ca53226c624061509fd
Benny24aa <harleyb101010@gmail.com>
2024-11-23 00:22:41 +0000

    establishing titles for both imagery and endoscopy tabs on healthboard analysis

--- NHSOpenDataDashboard/ui (sources)/Diagnostic Waiting Times Code/diagnostic waiting times ui.R
+++ NHSOpenDataDashboard/ui (sources)/Diagnostic Waiting Times Code/diagnostic waiting times ui.R
@@ -9,8 +9,20 @@ Diagnostics_section <- tabPanel(title = "Healthboard Breakdown",
                                             
                                             conditionalPanel(
                                               condition= 'input.diag_select == "endo"',
+                                              h1("Monthly Breakdown of Endoscopy Data at Health Board Level", style = "color: #336699 ; font-weight: 600"),
+                                              p("Coming soon")
                                               
                                               
+                                              ),
+                                            
+                                            conditionalPanel(
+                                              condition= 'input.diag_select == "imag"',
+                                              h1("Monthly Breakdown of Imagery Data at Health Board Level", style = "color: #336699 ; font-weight: 600"),
+                                              p("Coming soon")
                                               
-                                              ))))
+                                              
+                                            )
+                                            
+                                            
+                                            )))
 
