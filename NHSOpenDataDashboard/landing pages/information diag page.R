d87bfb6b4c6a387216e863dca67f2fe389493bc7
Benny24aa <harleyb101010@gmail.com>
2024-12-01 15:05:58 +0000

    graph added to landing page

--- NHSOpenDataDashboard/landing pages/information diag page.R
+++ NHSOpenDataDashboard/landing pages/information diag page.R
@@ -12,10 +12,10 @@ Diagnostic_Landing_Page_UI <- tabPanel(title = "Information Page",
                                          column(12,
                                                 h4(tags$b("What is in this section?" , style = "color:  #336699 ; font-weight: 600")),
                                                 p(""), 
-                                                h4(tags$b("Type of Data Explored?", style = "color:  #336699 ; font-weight: 600" )),
-                                                p(""),
+                                                h4(tags$b("Scotland Wide Graph", style = "color:  #336699 ; font-weight: 600" )),
+                                                fluidRow(
+                                                  column(3, plotlyOutput("", width = "300%", height = "600px"))),
                                                 h4(tags$b(" Disclosure and Data Security Statement", style = "color:  #336699 ; font-weight: 600")),
                                                 p("All content is available under the Open Government License V3.0, and is available on NHS Scotland Open Data except where otherwise stated. If you need any assistance with this, please visit the UK Government Website for more information regarding the Open Government License. This dashboard is not a representive of the NHS and therefore is not an official source of information.")),
                                        )#End of Fluid Row
-                                       
 ) #End of TabPanel
