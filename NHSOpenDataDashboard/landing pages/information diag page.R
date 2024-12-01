ef2ec4cdf50b8b0b54f07a8f9f30da23c78d9cae
Benny24aa <harleyb101010@gmail.com>
2024-12-01 15:06:19 +0000

    added grpahs to landing page server

--- NHSOpenDataDashboard/landing pages/information diag page.R
+++ NHSOpenDataDashboard/landing pages/information diag page.R
@@ -14,7 +14,7 @@ Diagnostic_Landing_Page_UI <- tabPanel(title = "Information Page",
                                                 p(""), 
                                                 h4(tags$b("Scotland Wide Graph", style = "color:  #336699 ; font-weight: 600" )),
                                                 fluidRow(
-                                                  column(3, plotlyOutput("", width = "300%", height = "600px"))),
+                                                  column(3, plotlyOutput("scot_diagnsotics_land_page_graph", width = "300%", height = "600px"))),
                                                 h4(tags$b(" Disclosure and Data Security Statement", style = "color:  #336699 ; font-weight: 600")),
                                                 p("All content is available under the Open Government License V3.0, and is available on NHS Scotland Open Data except where otherwise stated. If you need any assistance with this, please visit the UK Government Website for more information regarding the Open Government License. This dashboard is not a representive of the NHS and therefore is not an official source of information.")),
                                        )#End of Fluid Row
