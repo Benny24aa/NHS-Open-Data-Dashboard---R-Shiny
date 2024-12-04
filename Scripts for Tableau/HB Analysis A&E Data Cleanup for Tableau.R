d523a29bf033424c2f40709e5e6d9e7721205fca
Benny24aa <harleyb101010@gmail.com>
2024-12-04 01:31:14 +0000

    when data added to tableau ae script

--- Scripts for Tableau/HB Analysis A&E Data Cleanup for Tableau.R
+++ Scripts for Tableau/HB Analysis A&E Data Cleanup for Tableau.R
@@ -175,3 +175,12 @@ merged_ae_demographic_data<- bind_rows(ae_age_analysis, deprivation_ae_analysis,
 
 hbcategorytype <- merged_ae_demographic_data %>% 
   distinct(HBName,type)
+
+accidentandemergencydatawhendata<- get_resource(res_id = "022c3b27-6a58-48dc-8038-8f1f93bb0e78") %>% 
+  select(-Country,-TreatmentLocation, -Week) %>% 
+  filter(Month > '202212') %>% 
+  filter(DepartmentType == "Emergency Department") %>% 
+  select(-DepartmentType) %>% 
+  group_by(HBT, Month, Hour, Day, InOut ) %>% 
+  summarise(NumberOfAttendances = sum(NumberOfAttendances), .groups = 'drop') %>% 
+  rename(HBName = HBT)
