3ece64367df574e8fe41f17a25a74d257ab5fd8c
Benny24aa <harleyb101010@gmail.com>
2024-12-04 01:29:33 +0000

    section 1 of analytics added for tableau for ae

---
+++ Scripts for Tableau/HB Analysis A&E Data Cleanup for Tableau.R
@@ -0,0 +1,15 @@
+### Weekly Accident and Emergency Data taken from Open Data through PHS Open Data Library
+accidentandemergencydata <- get_resource(res_id = "a5f7ca94-c810-41b5-a7c9-25c18d43e5a4")
+
+accidentandemergencydata_cleaned <- accidentandemergencydata |>
+  select(-Country,-DepartmentType)|> ### Removed both columns as both have the same value for all rows
+  rename(HospitalCode = TreatmentLocation)|> ### To allow for future left_join with Hospital reference file
+  rename(HB = HBT)  ### To allow for future left_join with HBT reference file
+
+
+accidentandemergencydata_cleaned <- left_join(accidentandemergencydata_cleaned, Hospital_Lookup_Final, by = "HospitalCode") %>%  ### Left Join for Hospital Names
+  select(-HospitalCode) #### Removing hospital code as no longer needed anymore so removed to reduce size of dataframe
+
+accidentandemergencydata_cleaned <- left_join(accidentandemergencydata_cleaned, HB_Lookup_Cleaned, by = "HB") %>% #### left join for Healthcode Name
+  select(-HB) # %>%   #### Removing HB code as not needed anymore so removed to reduce size of dataframe
+# mutate(HBName = gsub("NHS ", "", HBName)) #Reducing the Size of the dataset by removing NHS from every column
